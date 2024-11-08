19a0e25766543cba035825f6bf2387711a93f2b9
iamebenezer <abegundeebenezer2004@gmail.com>
2024-11-08 23:42:37 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import './App.css'
 import chair from './assets/images/hero-img.png'
+import productOne from './assets/images/p-1.jpg'
+import productTwo from './assets/images/p-2.jpg'
+import productThree from './assets/images/p-3.jpg'
+import productFour from './assets/images/p-4.jpg'
+import productFive from './assets/images/p-5.jpg'
 import heroImg from './assets/images/hero-img.png'
 import { FaPhoneAlt } from "react-icons/fa";
 import { FaChevronRight } from "react-icons/fa";
@@ -78,9 +83,24 @@ function App() {
         {/* Cards */}
 
         <div className="grid md:grid-cols-4 grid-cols-1 py-12 gap-10">
-          <div className="h-auto py-4 w-fit text-center rounded-3xl shadow-xl border border-[#b99c88] bg-[#f5f0ef]">
+          <div className="h-fit py-4 w-fit text-center rounded-3xl shadow-xl border border-[#b99c88] bg-[#f5f0ef]">
               <div>
-                <img src={chair} alt="" />
+                <img src={productFive} alt="" />
+              </div>
+              {/* Name and Price */}
+              <div className="px-6 h-fit flex justify-between items-center">
+                <div className="">
+                  <span className='font-btn-texts md:text-xl'>Chair</span>
+                </div>
+                <div>
+                  <span className='font-btn-text md:text-xl'>#35,000</span>
+                </div>
+              </div>
+              <a href="" className=' font-btn-text bg-[#2b1100] my-3 text-white px-6 py-1 rounded-md'>Order Now</a>
+          </div>
+          <div className="h-fit py-4 w-fit text-center rounded-3xl shadow-xl border border-[#b99c88] bg-[#f5f0ef]">
+              <div>
+                <img src={productTwo} alt="" />
               </div>
               {/* Name and Price */}
               <div className="px-6  flex justify-between items-center">
@@ -93,9 +113,9 @@ function App() {
               </div>
               <a href="" className=' font-btn-text bg-[#2b1100] my-3 text-white px-6 py-1 rounded-md'>Order Now</a>
           </div>
-          <div className="h-auto py-4 w-fit text-center rounded-3xl shadow-xl border border-[#b99c88] bg-[#f5f0ef]">
+          <div className="h-fit py-4 w-fit text-center rounded-3xl shadow-xl border border-[#b99c88] bg-[#f5f0ef]">
               <div>
-                <img src={chair} alt="" />
+                <img src={productOne} alt="" />
               </div>
               {/* Name and Price */}
               <div className="px-6  flex justify-between items-center">
@@ -108,24 +128,9 @@ function App() {
               </div>
               <a href="" className=' font-btn-text bg-[#2b1100] my-3 text-white px-6 py-1 rounded-md'>Order Now</a>
           </div>
-          <div className="h-auto py-4 w-fit text-center rounded-3xl shadow-xl border border-[#b99c88] bg-[#f5f0ef]">
+          <div className="h-fit py-4 w-fit text-center rounded-3xl shadow-xl border border-[#b99c88] bg-[#f5f0ef]">
               <div>
-                <img src={chair} alt="" />
-              </div>
-              {/* Name and Price */}
-              <div className="px-6  flex justify-between items-center">
-                <div className="">
-                  <span className='font-btn-texts md:text-xl'>Chair</span>
-                </div>
-                <div>
-                  <span className='font-btn-text md:text-xl'>#35,000</span>
-                </div>
-              </div>
-              <a href="" className=' font-btn-text bg-[#2b1100] my-3 text-white px-6 py-1 rounded-md'>Order Now</a>
-          </div>
-          <div className="h-auto py-4 w-fit text-center rounded-3xl shadow-xl border border-[#b99c88] bg-[#f5f0ef]">
-              <div>
-                <img src={chair} alt="" />
+                <img src={productThree} alt="" />
               </div>
               {/* Name and Price */}
               <div className="px-6  flex justify-between items-center">
@@ -211,12 +216,7 @@ function App() {
        
        <div className="grid grid-cols-1 gap-x-12 md:grid-cols-2 items-center py-10">
         <div>
-        <Iframe url="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=gwarinpa%20ABUJA+(Nature%20Funiture%20and%20Inferior)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
-        width=""
-        height=""
-        id=""
-        className="w-full h-96 flex justify-center items-center"
-       />
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15857.994507269892!2d7.5238636!3d6.45833!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1044a5b32dafc413%3A0xbc1e1921c4a3ffd!2sNatureWorld%20Interior%20and%20Furniture!5e0!3m2!1sen!2sng!4v1731105710093!5m2!1sen!2sng" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
      
      
         </div>
@@ -226,7 +226,7 @@ function App() {
 
             <li className=' font-texts'><FaLocationPin className='inline-flex' size={20}/> 4th Avenue Funiture Plaza SO9</li>
             <li  className=' font-texts'><FaGlobe className='inline-flex' size={20}/> www.naturefunitures.com</li>
-            <li  className=' font-texts'><FaPhone className='inline-flex' size={20}/>  +234 8093248943</li>
+            <li  className=' font-texts'><FaPhone className='inline-flex' size={20}/>  +234 09134537728</li>
             <li  className=' font-texts'><IoMdMail className='inline-flex' size={20}/> naturefuniture@gmail.com</li>
           </ul>
         </div>
@@ -244,7 +244,7 @@ function App() {
             <h3 className='font-head md:text-2xl'>Nature Funiture's</h3>
             <p className='font-para md:text-xl'>Nature World of Furniture and interiors.</p>
             </div>
-            <div>
+            <div className=' hidden md:flex'>
                 <span className='font-head underline underline-offset-4 md:text-lg'>Links</span>
                 <ul>
                   <li className='font-para text-xl'> <a href="">Home</a></li>
@@ -260,7 +260,7 @@ function App() {
           <div className="flex flex-col mt-6 justify-center items-center">
           <div className='inline-flex space-x-3'>
             <FaInstagram size={20} />
-            <FaTiktok size={20} />
+         
             <FaWhatsapp size={20} />
             </div>
           <p className='flex justify-center items-center font-para'>All Rights Resserved. 2024</p>
